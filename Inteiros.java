00d3686d8eea296cdb0c5fe985b31d74f123d3c4
CesarOli <cesaraleoliveira@gmail.com>
2023-12-15 11:27:08 -0300

    Tipos de Variavéis.

---
+++ Inteiros.java
@@ -0,0 +1,21 @@
+public class Inteiros {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicação = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicação);
+        System.out.println(divisao);
+
+
+    }
+
+}
